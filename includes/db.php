ffecd4ca5eb75d642afbcaa1137fa738c3cab9a5
paccyhabi <paccyhabi@gmail.com>
2024-10-01 12:00:52 +0200

    kjkj

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = getenv('MYSQL_ADDON_HOST');
-$db = getenv('MYSQL_ADDON_DB');
-$user = getenv('MYSQL_ADDON_USER');
-$pass = getenv('MYSQL_ADDON_PASSWORD');
+$host = "bust7tcar4t2dsotbnt3-mysql.services.clever-cloud.com";
+$db = "bust7tcar4t2dsotbnt3";
+$user = "ujp26f3ewgomzjur";
+$pass = "2hmrqxpnyxkRHUDLDBUW";
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
